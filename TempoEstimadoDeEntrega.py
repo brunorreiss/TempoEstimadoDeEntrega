83b4cdb1b5d3da304399e5bf882dd3a4cfc3b42a
Bruno Reis <83921326+brunorreiss@users.noreply.github.com>
2023-07-03 23:14:25 -0300

    Create TempoEstimadoDeEntrega.py

---
+++ TempoEstimadoDeEntrega.py
@@ -0,0 +1,6 @@
+nomeRestaurante = input()
+tempoEstimadoEntrega = int(input())
+
+# Utilize a interpolação de strings para formatar a mensagem de saída
+mensagem = f"O restaurante {nomeRestaurante} entrega em {tempoEstimadoEntrega} minutos."
+print(mensagem)
